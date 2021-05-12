f4c9c437de9d0a9329b746ca9091a9d79c09439c
Max16 <max16@naver.com>
2021-05-12 09:57:28 +0900

    #2.2 casheFOnts

--- App.js
+++ App.js
@@ -1,7 +1,38 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { AppLoading } from "expo";
+import React, { useState } from "react";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { Image, Text } from "react-native";
+import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
 
+const casheImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+
+const casheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 export default function App() {
-  return <AppLoading />;
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = () => {
+    const images = casheImages([
+      "https://images.unsplash.com/photo-1620745109479-d77427e76804?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0N3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      require("./assets/splash.png"),
+    ]);
+    const fonts = casheFonts([Ionicons.font]);
+    return Promise.all([...images, ...fonts]);
+  };
+  const onFinish = () => setIsReady(true);
+  return isReady ? (
+    <Text>Ready!</Text>
+  ) : (
+    <AppLoading
+      startAsync={loadAssets}
+      onFinish={onFinish}
+      onError={console.error}
+    />
+  );
 }
