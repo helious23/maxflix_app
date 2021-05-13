4c01565082cbd697eecb9576e069ccfcdf8f0b6e
Max16 <max16@naver.com>
2021-05-13 12:28:33 +0900

    #4.3 Slider Content

--- screens/Movies/MoviesPresentor.js
+++ screens/Movies/MoviesPresentor.js
@@ -1,13 +1,20 @@
 import React from "react";
 import styled from "styled-components/native";
 import Swiper from "react-native-web-swiper";
-import { ActivityIndicator, Dimensions } from "react-native";
+import { ActivityIndicator, Dimensions, View } from "react-native";
 import Slide from "../../components/Movies/Slide";
+
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("screen");
 
 const Container = styled.View`
   flex: 1;
   background-color: black;
   justify-content: center;
+`;
+
+const SlideContainer = styled.View`
+  width: ${WIDTH}px;
+  height: ${HEIGHT / 4}px;
 `;
 
 export default ({ loading, nowPlaying }) => (
@@ -16,18 +23,20 @@ export default ({ loading, nowPlaying }) => (
       <ActivityIndicator size="small" color="white" /> // loading : true 일 때 모래시계
     ) : (
       <>
-        <Swiper controlsEnabled={false} loop timeout={3}>
-          {nowPlaying.map((movie) => (
-            <Slide
-              key={movie.id}
-              id={movie.id}
-              title={movie.original_title}
-              overview={movie.overview}
-              votes={movie.vote_average}
-              backgroundImage={movie.backdrop_path}
-            />
-          ))}
-        </Swiper>
+        <SlideContainer>
+          <Swiper controlsEnabled={false} loop timeout={3} height>
+            {nowPlaying.map((movie) => (
+              <Slide
+                key={movie.id}
+                id={movie.id}
+                title={movie.original_title}
+                overview={movie.overview}
+                votes={movie.vote_average}
+                backgroundImage={movie.backdrop_path}
+              />
+            ))}
+          </Swiper>
+        </SlideContainer>
       </>
     )}
   </Container>
