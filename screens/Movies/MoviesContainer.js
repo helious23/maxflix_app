d19d269a4fad7c966a99a5ac9c1390ac44562a1f
Max16 <max16@naver.com>
2021-05-13 09:00:37 +0900

    #4.0 Home Slider

--- screens/Movies/MoviesContainer.js
+++ screens/Movies/MoviesContainer.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
-import { movieApi } from "../api";
+import { movieApi } from "../../api";
+import MoviesPresneter from "./MoviesPresentor";
 
 export default () => {
   const [movies, setMovies] = useState({
+    loading: true,
     nowPlaying: [],
     popular: [],
     upcoming: [],
@@ -16,6 +18,7 @@ export default () => {
     const [popular, popularError] = await movieApi.popular();
     const [upcoming, upcomingError] = await movieApi.upcoming();
     setMovies({
+      loading: false,
       nowPlaying,
       popular,
       upcoming,
@@ -27,9 +30,5 @@ export default () => {
   useEffect(() => {
     getData();
   }, []);
-  return (
-    <View style={{ backgroundColor: "black", flex: 1 }}>
-      <Text style={{ color: "white" }}>{movies.nowPlaying?.length}</Text>
-    </View>
-  );
+  return <MoviesPresneter />;
 };
