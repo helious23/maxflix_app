52309c81133eeeac4bb2d54a47832df097d25845
Max16 <max16@naver.com>
2021-05-13 11:26:43 +0900

    #4.2 Slider Background

--- screens/Movies/MoviesPresentor.js
+++ screens/Movies/MoviesPresentor.js
@@ -2,8 +2,7 @@ import React from "react";
 import styled from "styled-components/native";
 import Swiper from "react-native-web-swiper";
 import { ActivityIndicator, Dimensions } from "react-native";
-
-const { width, height } = Dimensions.get("screen");
+import Slide from "../../components/Movies/Slide";
 
 const Container = styled.View`
   flex: 1;
@@ -11,32 +10,25 @@ const Container = styled.View`
   justify-content: center;
 `;
 
-const Header = styled.View`
-  width: 100%;
-  height: ${height / 3}px;
-`;
-
-const Section = styled.View`
-  background-color: skyblue;
-  height: 100%;
-`;
-
-const Text = styled.Text``;
-
 export default ({ loading, nowPlaying }) => (
   <Container>
     {loading ? (
-      <ActivityIndicator size="small" color="white" />
+      <ActivityIndicator size="small" color="white" /> // loading : true 일 때 모래시계
     ) : (
-      <Header>
+      <>
         <Swiper controlsEnabled={false} loop timeout={3}>
           {nowPlaying.map((movie) => (
-            <Section key={movie.id}>
-              <Text>{movie.original_title}</Text>
-            </Section>
+            <Slide
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              overview={movie.overview}
+              votes={movie.vote_average}
+              backgroundImage={movie.backdrop_path}
+            />
           ))}
         </Swiper>
-      </Header>
+      </>
     )}
   </Container>
 );
