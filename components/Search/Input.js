04801c49f11be8f0c847f969e5ae3e199e4fa7bd
Max16 <max16@naver.com>
2021-05-14 13:37:37 +0900

    #6.0 Search input

---
+++ components/Search/Input.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components/native";
+import PropTypes from "prop-types";
+import { onChange } from "react-native-reanimated";
+
+const TextInput = styled.TextInput`
+  background-color: white;
+`;
+
+const Input = ({ placeholder, value, onChange, onSubmit }) => (
+  <TextInput
+    placeholder={placeholder}
+    value={value}
+    onChangeText={onChange}
+    onSubmitEditing={onSubmit}
+    returnKeyType={"search"}
+    autoCorrect={false}
+    autoFocus={true}
+    keyboardAppearance={"dark"}
+  />
+);
+
+Input.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default Input;
---
+++ screens/Search/SearchContainer.js
@@ -0,0 +1,4 @@
+import React from "react";
+import SearchPresentor from "./SearchPresentor";
+
+export default () => <SearchPresentor />;
