e1398b8469e08e5a689a2ce98ce2540fe3340a8b
Max16 <max16@naver.com>
2021-05-14 16:57:18 +0900

    #7.1 Detail Styles One

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,12 +1,33 @@
-import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
 import React from "react";
 import { useEffect } from "react";
-import { View, Text } from "react-native";
+import { Dimensions } from "react-native";
+import styled from "styled-components/native";
+import { apiImage } from "../api";
+import Poster from "../components/Poster";
+import ScrollContainer from "../components/ScrollContainer";
+import Votes from "../components/Votes";
+
+const BG = styled.Image`
+  width: 100%;
+  height: ${Dimensions.get("window").height / 3}px;
+  opacity: 0.4;
+  position: absolute;
+`;
+
+const Header = styled.View``;
+
+const Container = styled.View``;
+
+const Title = styled.Text`
+  color: white;
+`;
+
+const Info = styled.View``;
 
 export default ({
   navigation,
   route: {
-    params: { id, title },
+    params: { id, title, backgroundImage, poster, votes },
   },
 }) => {
   useEffect(() => {
@@ -16,8 +37,17 @@ export default ({
   }, []);
 
   return (
-    <View>
-      <Text>{id}</Text>
-    </View>
+    <ScrollContainer loading={false}>
+      <Header>
+        <BG source={{ uri: apiImage(backgroundImage, "-") }} />
+        <Container>
+          <Poster url={poster} />
+          <Info>
+            <Title>{title}</Title>
+            <Votes votes={votes} />
+          </Info>
+        </Container>
+      </Header>
+    </ScrollContainer>
   );
 };
