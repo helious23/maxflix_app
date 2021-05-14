222f9590c1105b3d0e779ed899d75eb8121f0200
Max16 <max16@naver.com>
2021-05-14 13:52:03 +0900

    #6.1 Searching Logic

--- screens/Search/SearchContainer.js
+++ screens/Search/SearchContainer.js
@@ -1,4 +1,33 @@
 import React from "react";
+import { useState } from "react";
+import { movieApi, tvApi } from "../../api";
 import SearchPresentor from "./SearchPresentor";
 
-export default () => <SearchPresentor />;
+export default () => {
+  const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState({
+    movies: [],
+    shows: [],
+    moviesError: null,
+    showsError: null,
+  });
+  const onChange = (text) => setKeyword(text);
+  const search = async () => {
+    const [movies, moviesError] = await movieApi.search(keyword);
+    const [shows, showsError] = await tvApi.search(keyword);
+    setResults({
+      movies,
+      shows,
+      moviesError,
+      showsError,
+    });
+  };
+  return (
+    <SearchPresentor
+      {...results}
+      onChange={onChange}
+      onSubmit={search}
+      keyword={keyword}
+    />
+  );
+};
